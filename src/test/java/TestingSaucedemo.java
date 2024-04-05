26a29d5640c40df716f5d5498e4ceee1b48fd248
Kaspis <kasparas.ziuraitis@gmail.com>
2024-04-05 10:44:14 +0300

    tetPriceEquality

--- src/test/java/TestingSaucedemo.java
+++ src/test/java/TestingSaucedemo.java
@@ -45,6 +45,52 @@ public class TestingSaucedemo {
 //        VIETOJ WebElement galima naudoti var
 
 
+    @Test
+    public void TestPriceEquality() {
+
+        _globalDriver.findElement(By.id("user-name")).sendKeys("standard_user");// user
+        _globalDriver.findElement(By.id("password")).sendKeys("secret_sauce");// password
+        _globalDriver.findElement(By.id("login-button")).click(); //login
+        _globalDriver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/div/div/div[1]/div[2]/div[2]/button")).click();//add to cart
+        _globalDriver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/div/div/div[2]/div[2]/div[2]/button")).click();//add to cart
+        _globalDriver.findElement(By.xpath("/html/body/div/div/div/div[1]/div[1]/div[3]")).click();
+        _globalDriver.findElement(By.id("checkout")).click();// checkou
+        _globalDriver.findElement(By.id("first-name")).sendKeys("vaida");//ckeckout
+        _globalDriver.findElement(By.id("last-name")).sendKeys("ziustr");//pavarde
+        _globalDriver.findElement(By.id("postal-code")).sendKeys("44172"); //pasto kodas
+        _globalDriver.findElement(By.id("continue")).click();//testi pereinant oprie apmokejimo
+
+        String price1= _globalDriver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/div[1]/div[3]/div[2]/div[2]/div")).getText().replace("$", "");
+        String price2= _globalDriver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/div[1]/div[4]/div[2]/div[2]/div")).getText().replace("$", "");
+        double priceInPage = Double.parseDouble(price1) + Double.parseDouble(price2); //suma turiu uzrasyti.konvertuoju i double
+
+        String priceInCart = _globalDriver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/div[2]/div[6]")).getText().replace("Item total: $", "");
+
+        Assert.assertEquals(Double.parseDouble(priceInCart), priceInPage);
+
+//nesiguna patikrinti kainu
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
