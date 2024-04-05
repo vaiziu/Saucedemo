58c6b6866c8a5bc6326860f817e582b98ad1fe14
Kaspis <kasparas.ziuraitis@gmail.com>
2024-04-05 12:39:09 +0300

    test first name

--- src/test/java/TestingSaucedemo.java
+++ src/test/java/TestingSaucedemo.java
@@ -34,7 +34,7 @@ public class TestingSaucedemo {
         _globalDriver.findElement(By.id("last-name")).sendKeys("ziustr");//pavarde
         _globalDriver.findElement(By.id("postal-code")).sendKeys("44172"); //pasto kodas
         _globalDriver.findElement(By.id("continue")).click();//testi pereinant oprie apmokejimo
-        _globalDriver.findElement(By.id("finish")).click();
+        _globalDriver.findElement(By.id("finish")).click();//finish
 
         WebElement itemBaught = _globalDriver.findElement(By.xpath("/html/body/div/div/div/div[2]/h2"));// VIETOJ WebElement galima naudoti var
         Assert.assertEquals(itemBaught.getText(), "Thank you for your order!");
@@ -60,25 +60,80 @@ public class TestingSaucedemo {
         _globalDriver.findElement(By.id("postal-code")).sendKeys("44172"); //pasto kodas
         _globalDriver.findElement(By.id("continue")).click();//testi pereinant oprie apmokejimo
 
-        String price1= _globalDriver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/div[1]/div[3]/div[2]/div[2]/div")).getText().replace("$", "");
-        String price2= _globalDriver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/div[1]/div[4]/div[2]/div[2]/div")).getText().replace("$", "");
+        String price1 = _globalDriver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/div[1]/div[3]/div[2]/div[2]/div")).getText().replace("$", "");
+        String price2 = _globalDriver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/div[1]/div[4]/div[2]/div[2]/div")).getText().replace("$", "");
         double priceInPage = Double.parseDouble(price1) + Double.parseDouble(price2); //suma turiu uzrasyti.konvertuoju i double
 
         String priceInCart = _globalDriver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/div[2]/div[6]")).getText().replace("Item total: $", "");
-
         Assert.assertEquals(Double.parseDouble(priceInCart), priceInPage);
+    }
 
-//nesiguna patikrinti kainu
+//        if ( _globalDriver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/div/div/div[1]/div[2]/div[2]/button")));{
+//            System.out.println("daiktas yra");
+//        }
+//        else () {
+//            System.out.println("neideta");
+//        }
 
+        @Test
 
+        public void ChangeingItemQuantityInCart () {
+            _globalDriver.findElement(By.id("user-name")).sendKeys("standard_user");// user
+            _globalDriver.findElement(By.id("password")).sendKeys("secret_sauce");// password
+            _globalDriver.findElement(By.id("login-button")).click(); //login
+            _globalDriver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/div/div/div[1]/div[2]/div[2]/button")).click();//add to cart
+            _globalDriver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/div/div/div[2]/div[2]/div[2]/button")).click();//add to cart
+            _globalDriver.findElement(By.xpath("/html/body/div/div/div/div[1]/div[1]/div[3]")).click();
+            _globalDriver.findElement(By.id("checkout")).click();// checkou
+            _globalDriver.findElement(By.id("first-name")).sendKeys("vaida");//ckeckout
+            _globalDriver.findElement(By.id("last-name")).sendKeys("ziustr");//pavarde
+            _globalDriver.findElement(By.id("postal-code")).sendKeys("44172"); //pasto kodas
+            _globalDriver.findElement(By.id("continue")).click();//testi pereinant oprie apmokejimo
+            _globalDriver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/div[1]/div[3]/div[1]")).sendKeys("15");
+
+            String ItemQuantityInCart= _globalDriver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/div[1]/div[3]/div[1]")).getText();
+            Assert.assertEquals(ItemQuantityInCart, "15"); //turi feilint testas nes nekeicia kiekio
+
+    }
+
+    @Test
+    public void TestingEmptyCart () {
+        _globalDriver.findElement(By.id("user-name")).sendKeys("standard_user");// user
+        _globalDriver.findElement(By.id("password")).sendKeys("secret_sauce");// password
+        _globalDriver.findElement(By.id("login-button")).click(); //login
+        _globalDriver.findElement(By.xpath("/html/body/div/div/div/div[1]/div[1]/div[3]/a")).click();// cart
+        _globalDriver.findElement(By.id("checkout")).click();// checkou
+        _globalDriver.findElement(By.id("first-name")).sendKeys("vaida");//ckeckout
+        _globalDriver.findElement(By.id("last-name")).sendKeys("ziustr");//pavarde
+        _globalDriver.findElement(By.id("postal-code")).sendKeys("44172"); //pasto kodas
+        _globalDriver.findElement(By.id("continue")).click();//testi pereinant oprie apmokejimo
+        _globalDriver.findElement(By.id("finish")).click();//finish
+
+        WebElement emptyCart = _globalDriver.findElement(By.xpath("/html/body/div/div/div/div[2]/h2"));// VIETOJ WebElement galima naudoti var
+        Assert.assertEquals(emptyCart.getText(), "sorry you have no items"); //kaip kitaip galima patikrinti
 
 
     }
 
 
+    @Test
+    public void TestingWrongFirstName () {
+        _globalDriver.findElement(By.id("user-name")).sendKeys("standard_user");// user
+        _globalDriver.findElement(By.id("password")).sendKeys("secret_sauce");// password
+        _globalDriver.findElement(By.id("login-button")).click(); //login
+        _globalDriver.findElement(By.xpath("/html/body/div/div/div/div[1]/div[1]/div[3]/a")).click();// cart
+        _globalDriver.findElement(By.id("checkout")).click();// checkou
+        _globalDriver.findElement(By.id("first-name")).sendKeys("***");//ckeckout
+        _globalDriver.findElement(By.id("last-name")).sendKeys("ziustr");//pavarde
+        _globalDriver.findElement(By.id("postal-code")).sendKeys("44172"); //pasto kodas
+        _globalDriver.findElement(By.id("continue")).click();//testi pereinant oprie apmokejimo
+        _globalDriver.findElement(By.id("finish")).click();//finish
+
+        WebElement falseName = _globalDriver.findElement(By.xpath("/html/body/div/div/div/div[2]/h2"));//
+        Assert.assertEquals(falseName.getText(), "something wrong"); // test failed because it lets to finish having an empty cart
 
 
-
+    }
 
 
 
